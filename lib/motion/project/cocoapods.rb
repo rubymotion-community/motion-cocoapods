90751a6859f00e7d2e15e747608da20bf6b7040e
Francis Chong <francis@ignition.hk>
2012-05-16 01:32:49 +0800

    correct an issue caused by changing the config before creating the project

--- lib/motion/project/cocoapods.rb
+++ lib/motion/project/cocoapods.rb
@@ -59,14 +59,6 @@ module Motion::Project
       cp_config.rootspec = @podfile if cp_config.respond_to?(:rootspec) # CocoaPods 0.5.x backward compatibility
       cp_config.integrate_targets = false if cp_config.respond_to?(:integrate_targets) # CocoaPods 0.6 forward compatibility
       cp_config.project_root = Pathname.new(config.project_dir) + 'vendor'
-
-      @installer = Pod::Installer.new(@podfile)
-
-      # CocoaPods 0.5.x backward compatibility
-      if config.deployment_target && @installer.project.respond_to?(:build_configuration)
-        @installer.project.build_configuration("Debug").buildSettings["IPHONEOS_DEPLOYMENT_TARGET"]   = config.deployment_target
-        @installer.project.build_configuration("Release").buildSettings["IPHONEOS_DEPLOYMENT_TARGET"] = config.deployment_target
-      end
     end
 
     def dependency(*name_and_version_requirements, &block)
@@ -78,13 +70,21 @@ module Motion::Project
     end
 
     def pods_installer
-      @installer
+      @installer ||= Pod::Installer.new(@podfile)
     end
 
     # For now we only support one Pods target, this will have to be expanded
     # once we work on more spec support.
     def install!
-      pods_installer.install!
+      @installer = pods_installer
+
+      # CocoaPods 0.5.x backward compatibility
+      if @config.deployment_target && @installer.project.respond_to?(:build_configuration)
+        @installer.project.build_configuration("Debug").buildSettings["IPHONEOS_DEPLOYMENT_TARGET"]   = @config.deployment_target
+        @installer.project.build_configuration("Release").buildSettings["IPHONEOS_DEPLOYMENT_TARGET"] = @config.deployment_target
+      end
+
+      @installer.install!
 
       @config.vendor_project('vendor/Pods', :xcode,
         :target => 'Pods',
