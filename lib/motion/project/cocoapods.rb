ed31fd5f3476feb9336d749b493a104618caa693
Watson <watson1978@gmail.com>
2013-05-15 08:26:30 +0900

    1.3.2

--- lib/motion/project/cocoapods.rb
+++ lib/motion/project/cocoapods.rb
@@ -57,7 +57,7 @@ module Motion::Project
   #---------------------------------------------------------------------------#
 
   class CocoaPods
-    VERSION   = '1.3.1'
+    VERSION   = '1.3.2'
     PODS_ROOT = 'vendor/Pods'
 
     attr_accessor :podfile
